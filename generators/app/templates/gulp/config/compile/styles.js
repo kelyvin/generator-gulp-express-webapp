4a92fd82182fff28704f74e8b1b97ad81966c1f1
tdaff <tdaff@digitalink.com.au>
2016-11-15 09:56:14 +1100

    Changed ruby-sass to nodesass

--- generators/app/templates/gulp/config/compile/styles.js
+++ generators/app/templates/gulp/config/compile/styles.js
@@ -5,10 +5,7 @@ var $ = require('gulp-load-plugins')();
  gulp.task('styles', function () {
   return gulp.src('app/styles/*.scss')
     .pipe($.plumber())
-    .pipe($.rubySass({
-      style: 'expanded',
-      precision: 10
-    }))
+    .pipe($.sass().on('error', $.sass.logError))
     .pipe(cssImport())
     .pipe($.autoprefixer({browsers: ['last 1 version']}))
     .pipe(gulp.dest('.tmp/styles'));
